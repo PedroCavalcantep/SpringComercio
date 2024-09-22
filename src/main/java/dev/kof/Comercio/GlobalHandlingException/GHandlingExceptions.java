68212fcb8376ad7ad6685ede9ceaf0d4e1ac3fef
EsauDM <esau.ccbf@gmail.com>
2024-09-21 21:33:05 -0300

    Mais algumas validações

--- src/main/java/dev/kof/Comercio/GlobalHandlingException/GHandlingExceptions.java
+++ src/main/java/dev/kof/Comercio/GlobalHandlingException/GHandlingExceptions.java
@@ -1,7 +1,10 @@
 package dev.kof.Comercio.GlobalHandlingException;
 
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -31,6 +34,43 @@ public class GHandlingExceptions {
         body.put("timestamp", LocalDateTime.now());
         body.put("errors", errors);
         return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<Object> handleNotValidMethod(HttpRequestMethodNotSupportedException ex){
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("message", "Método HTTP inválido: " + ex.getMethod());
+        return new ResponseEntity<>(body, HttpStatus.METHOD_NOT_ALLOWED);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<Object> handleConstraintViolation(ConstraintViolationException ex){
+        Map<String, String> errors = new HashMap<>();
+        ex.getConstraintViolations().forEach(violation -> {
+            String field = violation.getPropertyPath().toString();
+            String message = violation.getMessage();
+            errors.put(field, message);
+        });
+
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("errors", errors);
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<Object> handleDataIntegrityViolation(DataIntegrityViolationException ex){
+        String errorMessage = "Erro: O login informado já existe.";
+
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", HttpStatus.CONFLICT.value());
+        body.put("error", "Data Integrity Violation");
+        body.put("message", errorMessage);
+        body.put("details", ex.getMostSpecificCause().getMessage());
+
+        return new ResponseEntity<>(body, HttpStatus.CONFLICT);
     }
 
 }
